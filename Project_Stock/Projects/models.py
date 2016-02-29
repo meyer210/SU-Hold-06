3c762972fb5e5975cc13fe314b400df85cec7f7f
Volerous <kg.mcd@hotmail.com>
2016-02-29 17:45:24 +0100

    changes and added author

--- Project_Stock/Projects/models.py
+++ Project_Stock/Projects/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=2000)
+    project_text = models.CharField(max_length=2000)
     description = models.TextField()
     author = models.ForeignKey(User, null=True, blank=True)
     pub_date = timezone.now()
--- Project_Stock/Projects/views.py
+++ Project_Stock/Projects/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Project
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello")
-def detail(request, Project_id):
-    return HttpResponse("Porject #%s" % Project_id)
+
+def detail(request, project_id):
+    output = get_object_or_404(Project, pk=project_id)
+    return render(request, 'Projects/detail.html', {'Project' : output})
