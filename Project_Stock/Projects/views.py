f4f9256b3cf7ea943df1c36c83f18e178cd38ebc
Volerous <kg.mcd@hotmail.com>
2016-02-29 12:20:55 +0100

    Add extra URLs

--- Project_Stock/Projects/views.py
+++ Project_Stock/Projects/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("hello")
+def detail(request, Project_id):
+    return HttpResponse("Porject #%s" % Project_id)
